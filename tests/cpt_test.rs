8e5b1770f8215d25c2304383854f15553f3492f2
AlexAegis <alexaegis@gmail.com>
2019-11-26 23:54:03 +0100

    test

--- tests/cpt_test.rs
+++ tests/cpt_test.rs
@@ -7,19 +7,19 @@ fn cpt_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
 	let to = String::from("./example_to");
 	let mut data = std::collections::HashMap::<String, String>::new();
 	data.insert("foo".to_string(), "bar".to_string());
-	cpt(from, to, &data)?;
+	cpt(from, to.clone(), &data)?;
 
 	assert_eq!(
-		fs::read_to_string("./example_to/bar.txt")?.replace("\r\n", "\n"),
+		fs::read_to_string(to.clone() + "/bar.txt")?.replace("\r\n", "\n"),
 		"This will become bar: bar\nAnd this will stay as is: {{escaped}}\n"
 	);
 
 	assert_eq!(
-		fs::read_to_string("./example_to/foo/non-template.txt")?.replace("\r\n", "\n"),
+		fs::read_to_string(to.clone() + "/foo/non-template.txt")?.replace("\r\n", "\n"),
 		"unused: {{unused}}"
 	);
 
 	// Cleanup
-	fs::remove_dir_all("./example_to")?;
+	fs::remove_dir_all(&to)?;
 	Ok(())
 }
