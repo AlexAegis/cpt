d3b4943421e3165a7f2fbc73c4e1545aa295ba50
AlexAegis <alexaegis@gmail.com>
2019-11-27 00:30:02 +0100

    command line test

--- tests/cpt_test.rs
+++ tests/cpt_test.rs
@@ -1,25 +1,52 @@
 use cpt::cpt;
 use std::fs;
 
+use assert_cmd::prelude::*;
+use std::process::Command;
+
 #[test]
-fn cpt_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
-	let from = String::from("./example");
-	let to = String::from("./example_to");
-	let mut data = std::collections::HashMap::<String, String>::new();
-	data.insert("foo".to_string(), "bar".to_string());
-	cpt(from, to.clone(), &data)?;
+fn args_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	let from = "./example";
+	let to = "./example_to";
+
+	Command::cargo_bin("cpt")
+		.unwrap()
+		.args(&[from, to, r#"--json={ "foo": "bar" }"#])
+		.assert()
+		.success();
 
 	assert_eq!(
-		fs::read_to_string(to.clone() + "/bar.txt")?.replace("\r\n", "\n"),
+		fs::read_to_string(format!("{}/bar.txt", to))?.replace("\r\n", "\n"),
 		"This will become bar: bar\nAnd this will stay as is: {{escaped}}\n"
 	);
 
 	assert_eq!(
-		fs::read_to_string(to.clone() + "/foo/non-template.txt")?.replace("\r\n", "\n"),
+		fs::read_to_string(format!("{}/foo/non-template.txt", to))?.replace("\r\n", "\n"),
+		"unused: {{unused}}"
+	);
+
+	Ok(())
+}
+
+#[test]
+fn cpt_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	let from = "./example";
+	let to = "./example_to";
+	let mut data = std::collections::HashMap::<String, String>::new();
+	data.insert("foo".to_string(), "bar".to_string());
+	cpt(from.to_string(), to.to_string(), &data)?;
+
+	assert_eq!(
+		fs::read_to_string(format!("{}/bar.txt", to))?.replace("\r\n", "\n"),
+		"This will become bar: bar\nAnd this will stay as is: {{escaped}}\n"
+	);
+
+	assert_eq!(
+		fs::read_to_string(format!("{}/foo/non-template.txt", to))?.replace("\r\n", "\n"),
 		"unused: {{unused}}"
 	);
 
 	// Cleanup
-	fs::remove_dir_all(&to)?;
+	// fs::remove_dir_all(&to)?;
 	Ok(())
 }
