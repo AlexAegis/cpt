d1f5c888274d7cb6c3ae5a2ca6831f1ce45708fa
AlexAegis <alexaegis@gmail.com>
2019-11-26 20:32:05 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use cpt::args;
+
+use cpt::cpt;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let (from, to) = args()?;
+	println!("from {} to {}", from, to);
+	let mut data = std::collections::HashMap::<String, String>::new();
+	data.insert("world".to_string(), "sike".to_string());
+	cpt(from, to, Some(&data)).await?;
+	Ok(())
+}
