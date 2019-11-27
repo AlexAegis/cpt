edef14216f2c7f4230cf5db5bbdf6dcb86ff572b
AlexAegis <alexaegis@gmail.com>
2019-11-27 20:20:34 +0100

    relocated examples

--- tests/cpt_test.rs
+++ tests/cpt_test.rs
@@ -24,8 +24,8 @@ fn do_assert(to: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn args_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
-	let from_og = "./example".to_string();
-	let to_og = "./example_to_args".to_string();
+	let from_og = "./templates/example".to_string();
+	let to_og = "./templates/example_to_args".to_string();
 
 	let c = Cpt::<String, String>::from_args(Some(&Cpt::new(from_og.clone(), to_og.clone())))?;
 
@@ -37,8 +37,8 @@ fn args_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn cpt_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
-	let from = "./example";
-	let to = "./example_to_cpt";
+	let from = "./templates/example";
+	let to = "./templates/example_to_cpt";
 	let mut data = std::collections::HashMap::<String, String>::new();
 	data.insert("foo".to_string(), "bar".to_string());
 	cpt(from.to_string(), to.to_string(), data)?;
@@ -53,8 +53,8 @@ fn cpt_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn cp_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
-	let from = "./example";
-	let to = "./example_to_cp";
+	let from = "./templates/example";
+	let to = "./templates/example_to_cp";
 
 	cp(from.to_string(), to.to_string())?;
 
@@ -80,8 +80,8 @@ fn cp_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn bin_test() -> std::result::Result<(), Box<dyn std::error::Error>> {
-	let from = "./example";
-	let to = "./example_to_args";
+	let from = "./templates/example";
+	let to = "./templates/example_to_args";
 
 	Command::cargo_bin("cpt")
 		.unwrap()
